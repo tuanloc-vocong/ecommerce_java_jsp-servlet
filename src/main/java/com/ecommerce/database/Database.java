6d9985d2961c176c4228f7ae752a25540d7d7bab
tuanloc-vocong <vocong.tuanloc@gmail.com>
2024-01-15 02:11:43 +0700

    jdbc

--- src/main/java/com/ecommerce/database/Database.java
+++ src/main/java/com/ecommerce/database/Database.java
@@ -7,7 +7,7 @@ public class Database {
     public Connection getConnection(){
         Connection connection;
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce_ja_jsp_servlet", "root", "root");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce_java_jsp_servlet?useSSL=true&requireSSL=true", "root", "");
             return connection;
         } catch (Exception e) {
             System.out.println(e.getMessage());
